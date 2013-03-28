df376952b242a1b3dc563131d3ee1a0a3ae34285
Paul Salaets <psalaets@gmail.com>
2013-03-28 10:58:50 -0400

    starting

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Etroutes::Application.config.secret_token = 'adbc65c8f97bf035373a72f500fef0e03b0c618ff8e4039a55fe48f588af244614e29132a583c4a635f4be0244b254843fb1091581bc892adad5f2c757025d48'
