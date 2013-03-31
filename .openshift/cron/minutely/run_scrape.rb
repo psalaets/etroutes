9f7fc8592179a6ece21c52bf9f0fa4a489459bde
Paul Salaets <psalaets@gmail.com>
2013-03-31 10:32:07 -0400

    scrape a lot until this is figured out

--- .openshift/cron/minutely/run_scrape.rb
+++ .openshift/cron/minutely/run_scrape.rb
@@ -5,7 +5,7 @@ ENV['RAILS_ENV'] ||= 'production'
 # Move into repo base to bundler can find Gemfile
 basedir = ENV['OPENSHIFT_REPO_DIR']
 
-# Every 15 minutes
-if [0, 15, 30, 45].include? Time.now.min
+# Every 3 minutes
+if %w(3 6 9).include? Time.now.min.to_s[-1]
   puts `cd #{basedir} && bundle exec ruby ./bin/scrape.rb 2>&1`
 end
