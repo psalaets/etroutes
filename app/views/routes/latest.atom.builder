0cbe49db935e5aa94922bc407659efaf3a0c0b6e
Paul Salaets <psalaets@gmail.com>
2013-03-30 22:34:34 -0400

    make sure feed has everything ifttt needs

--- app/views/routes/latest.atom.builder
+++ app/views/routes/latest.atom.builder
@@ -3,7 +3,7 @@ atom_feed do |feed|
   feed.updated(@routes.first.created_at) if @routes.any?
 
   @routes.each do |route|
-    feed.entry(route, :url => route.url) do |entry|
+    feed.entry(route, :id => route.guid, :url => route.url) do |entry|
       entry.title('New Route')
       entry.content(RoutePresenter.new(route).to_tweet)
     end
--- test/functional/routes_controller_test.rb
+++ test/functional/routes_controller_test.rb
@@ -4,14 +4,8 @@ require 'rexml/document'
 class RoutesControllerTest < ActionController::TestCase
   include REXML
 
-  test "should get latest routes in atom feed" do
-    get :latest, {:format => 'atom'}
-    assert_response :success
-    assert assigns(:routes)
-  end
-
-  test "should have route info in atom feed" do
-    route = Route.new(
+  def setup
+    @route = Route.new(
       :grade    => '5.10a',
       :gym      => 'Earth Treks',
       :location => 'Rockville',
@@ -20,14 +14,56 @@ class RoutesControllerTest < ActionController::TestCase
       :set_by   => 'Bob',
       :url      => 'http://route.com/1234'
     )
-    route.types = [:lead, :toprope]
-    route.save
+    @route.types = [:lead, :toprope]
+    @route.save
+  end
 
+  test "should get latest routes in atom feed" do
+    get :latest, {:format => 'atom'}
+
+    assert_response :success
+    assert assigns(:routes)
+  end
+
+  test "should have route info in atom feed" do
     get :latest, {:format => 'atom'}
 
     xml = Document.new(@response.body)
-    content = xml.elements['//entry/content']
+    content = xml.elements['/feed/entry[1]/content']
 
     assert_match /The Climb/, content.text
+  end
+
+  test "IFTTT requirement: feed has top level title" do
+    get :latest, {:format => 'atom'}
+
+    xml = Document.new(@response.body)
+
+    assert xml.elements['/feed/title'].text
+  end
+
+  test "IFTTT requirement: entries have id" do
+    get :latest, {:format => 'atom'}
+
+    xml = Document.new(@response.body)
+
+    assert xml.elements['/feed/entry[1]/id'].text
+  end
+
+  test "IFTTT requirement: entries have date(s)" do
+    get :latest, {:format => 'atom'}
+
+    xml = Document.new(@response.body)
+
+    assert xml.elements['/feed/entry[1]/updated'].text
+    assert xml.elements['/feed/entry[1]/published'].text
+  end
+
+  test "IFTTT requirement: entries have url" do
+    get :latest, {:format => 'atom'}
+
+    xml = Document.new(@response.body)
+
+    assert xml.elements['/feed/entry[1]/link[@rel="alternate"]'].attributes['href']
   end
 end
