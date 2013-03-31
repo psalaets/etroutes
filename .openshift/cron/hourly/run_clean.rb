68981f21ecad6919c031cd3915a56814994de4c2
Paul Salaets <psalaets@gmail.com>
2013-03-31 10:04:04 -0400

    fixing job kickoff scripts

--- .openshift/cron/hourly/run_clean.rb
+++ .openshift/cron/hourly/run_clean.rb
@@ -1,3 +1,8 @@
 #!/usr/bin/env ruby
 
-puts `bundle exec ruby ENV['$OPENSHIFT_REPO_DIR']/bin/clean_old.rb 2>&1`
+ENV['RAILS_ENV'] ||= 'production'
+
+# Move into repo base to bundler can find Gemfile
+basedir = ENV['$OPENSHIFT_REPO_DIR']
+
+puts `cd #{basedir} && bundle exec ruby ./bin/clean_old.rb 2>&1`
--- .openshift/cron/minutely/run_scrape.rb
+++ .openshift/cron/minutely/run_scrape.rb
@@ -1,6 +1,11 @@
 #!/usr/bin/env ruby
 
+ENV['RAILS_ENV'] ||= 'production'
+
+# Move into repo base to bundler can find Gemfile
+basedir = ENV['$OPENSHIFT_REPO_DIR']
+
 # Every 10 minutes
 if Time.now.min.to_s[-1] == '0'
-  puts `bundle exec ruby ENV['$OPENSHIFT_REPO_DIR']/bin/scrape.rb 2>&1`
+  puts `cd #{basedir} && bundle exec ruby ./bin/scrape.rb 2>&1`
 end
