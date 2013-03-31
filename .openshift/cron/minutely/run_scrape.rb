746c0fdd9a89b9a42c288a5fcb4fc0825acd56e3
Paul Salaets <psalaets@gmail.com>
2013-03-31 13:21:49 -0400

    change scrape rate to more reasonable number

--- .openshift/cron/minutely/run_scrape.rb
+++ .openshift/cron/minutely/run_scrape.rb
@@ -5,7 +5,7 @@ ENV['RAILS_ENV'] ||= 'production'
 # Move into repo base to bundler can find Gemfile
 basedir = ENV['OPENSHIFT_REPO_DIR']
 
-# Every 3 minutes
-if %w(3 6 9).include? Time.now.min.to_s[-1]
+# Every 30 minutes
+if [0, 30].include? Time.now.min
   puts `cd #{basedir} && bundle exec ruby ./bin/scrape.rb 2>&1`
 end
