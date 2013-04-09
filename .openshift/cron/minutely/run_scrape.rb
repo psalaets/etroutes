483e2add1843a59cc69cb994cd66323b4cf5d530
Paul Salaets <psalaets@gmail.com>
2013-04-09 19:44:33 -0400

    slight increase again

--- .openshift/cron/minutely/run_scrape.rb
+++ .openshift/cron/minutely/run_scrape.rb
@@ -2,8 +2,8 @@
 
 ENV['RAILS_ENV'] ||= 'production'
 
-# Every 20 minutes
-if [0, 20, 40].include? Time.now.min
+# Every 15 minutes
+if [0, 15, 30, 45].include? Time.now.min
   # Move into repo base so bundler can find Gemfile
   basedir = ENV['OPENSHIFT_REPO_DIR']
   puts `cd #{basedir} && bundle exec ruby ./bin/scrape.rb 2>&1`
